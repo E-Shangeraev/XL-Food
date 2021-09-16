adf85ca537b8581ba0c75394077708360e77c0da
E-Shangeraev <e.shangeraev@gmail.com>
2021-09-16 15:19:21 +0700

    Исправляю отображение фотографий товаров в корзине

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ app.use(errorHandler)
 
 // ==== App Start On Production ====
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  app.use('/', express.static(path.join(__dirname, '..', 'client', 'build')))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
   })
 }
 
